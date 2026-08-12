868140d31851de39f92f00eafe7ad2785b8301ab
SRIPADA DHEERAJ <sripadadheeraj2025@gmail.com>
2026-08-12 06:50:04 +0530

    Switched to PostgreSQL

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'resume_analyzer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
